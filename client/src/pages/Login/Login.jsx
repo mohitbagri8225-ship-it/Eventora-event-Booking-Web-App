11a3aa2b41e0a6b6b5e74370e1c6b8f28c27311f
Mohit Bagri <mohitbagri8225@gmail.com>
2026-08-26 12:43:41 +0530

    fix cors configuration

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://eventora-event-booking-web-app-1.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
--- server/app.js
+++ server/app.js
@@ -5,11 +5,20 @@ import cookieParser from 'cookie-parser';
 //intialize express app
 const app = express();
  
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://eventora-event-booking-web-app-1.onrender.com/"
+];
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
-
 app.use(express.json());//parse incoming JSON requests
 app.use(cookieParser());//parse incoming cookies
 app.use(express.urlencoded({ extended: true }));//parse incoming URL-encoded requests
